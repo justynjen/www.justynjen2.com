5a48e27e8756a3e2a7bf51ea8796add854994ba8
justynjen <justyngourdin@gmail.com>
2017-07-07 00:50:52 -0600

    July.7
    more java

--- _site/Javascripts/main.js
+++ _site/Javascripts/main.js
@@ -1,21 +1,13 @@
-function initparticles(){
-  hearts();
-}
+$(document).ready(function(){
+  //jQuery HERE
 
-function hearts() {
-   $.each($(".particletext.hearts"), function(){
-      var heartcount = ($(this).width()/50)*5;
-      for(var i = 0; i <= heartcount; i++) {
-         var size = ($.rnd(60,120)/10);
-         $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
-      }
-   });
-}
+  $('h1').hide().delay(1500).fadeIn(1400);
 
-jQuery.rnd = function(m,n) {
-      m = parseInt(m);
-      n = parseInt(n);
-      return Math.floor( Math.random() * (n - m + 1) ) + m;
-}
 
-initparticles();
+
+
+
+
+
+
+});
