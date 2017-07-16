fefdabb7b98b022be4498f17e1e6159e6f1685f3
justynjen <justyngourdin@gmail.com>
2017-07-16 02:29:23 -0600

    July.16
    added slider java

--- EndlessGlow/javascript/main.js
+++ EndlessGlow/javascript/main.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
   console.log("Ready");
   // $('.no-fouc').removeClass('no-fouc');
+  $(".nextLink").on("click", function() {
+    var currentActiveLink = $(".imageShow");
+    var nextActiveImage = currentActiveImage.next();
+
+    currentActiveImage.removeClass("imageShow").addClass("imageHidden").css("z-index", -10);
+    nextActiveImage.addClass("imageShow").removeClass("imageHidden").css("z-index", 20);
+    $("#slides").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+    e.preventDefault();
+  });
 });
+
+
+
+
+
+
 
 // $('#slides').slidesjs({
 //   dots: true,
