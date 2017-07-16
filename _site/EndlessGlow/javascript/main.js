9676571eef7c736e82246585da02493e7c0d103f
justynjen <justyngourdin@gmail.com>
2017-07-16 01:50:26 -0600

    July.16
    attemptig to add jquery

--- _site/EndlessGlow/javascript/main.js
+++ _site/EndlessGlow/javascript/main.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-  $('.no-fouc').removeClass('no-fouc');
+  console.log("Ready");
+  // $('.no-fouc').removeClass('no-fouc');
 });
 
-$('#slides').slidejs({
-  dots: true,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 6000,
-  speed: 800,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-});
+// $('#slides').slidesjs({
+//   dots: true,
+//   infinite: true,
+//   autoplay: true,
+//   autoplaySpeed: 6000,
+//   speed: 800,
+//   slidesToShow: 1,
+//   adaptiveHeight: true,
+// });
