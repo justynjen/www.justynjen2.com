8ed533daacfc98b2d857c32212ca3749a3a675ca
justynjen <justyngourdin@gmail.com>
2017-07-16 23:31:01 -0600

    July.17
    changes to owl

--- EndlessGlow/javascript/main.js
+++ EndlessGlow/javascript/main.js
@@ -1,52 +1,6 @@
 $(document).ready(function() {
   console.log("Ready");
-  // $('.no-fouc').removeClass('no-fouc');
-//   $(".nextLink").on("click", function() {
-//     var currentActiveLink = $(".imageShow");
-//     var nextActiveImage = currentActiveImage.next();
-//
-//     if(nextActiveImage.length == 0)
-//     {
-//       nextActiveImage = $(".carouselInner .bubble").first();
-//     }
-//
-//     currentActiveImage.removeClass("imageShow").addClass("imageHidden").css("z-index", -10);
-//     nextActiveImage.addClass("imageShow").removeClass("imageHidden").css("z-index", 20);
-//     $(".carouselInner div").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
-//
-//     e.preventDefault();
-//   });
-//   $(".perviousLink").on("click", function() {
-//     var currentActiveLink = $(".imageShow");
-//     var nextActiveImage = currentActiveImage.next();
-//
-//     if(nextActiveImage.length == 0)
-//     {
-//       nextActiveImage = $(".carouselInner .bubble").last();
-//     }
-//
-//     currentActiveImage.removeClass("imageShow").addClass("imageHidden").css("z-index", -10);
-//     nextActiveImage.addClass("imageShow").removeClass("imageHidden").css("z-index", 20);
-//     $(".carouselInner div").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
-//
-//     e.preventDefault();
-//   });
-// });
+
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
 });
-
-
-
-
-
-
-// $('#slides').slidesjs({
-//   dots: true,
-//   infinite: true,
-//   autoplay: true,
-//   autoplaySpeed: 6000,
-//   speed: 800,
-//   slidesToShow: 1,
-//   adaptiveHeight: true,
-// });
