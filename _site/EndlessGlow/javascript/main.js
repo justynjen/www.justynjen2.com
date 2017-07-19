e2690246174c6e3cef3edf018a94fa88d6ca4640
justynjen <justyngourdin@gmail.com>
2017-07-19 04:28:00 -0600

    July19
    Added the pop up

--- _site/EndlessGlow/javascript/main.js
+++ _site/EndlessGlow/javascript/main.js
@@ -1,19 +1,51 @@
+// Wait for the document to be ready
 $(document).ready(function(){
+
+  // variables
+  var windowHeight = null,
+      scrollPosition = null;
+
+  // Initialize the carousel
   $(".owl-carousel").owlCarousel({
-    loop:true,
+   loop:true,
    margin:10,
    nav:true,
-   navText: ["",""],
-   responsive:{
-       0:{
-           items:1
-       },
-       600:{
-           items:1
-       },
-       1000:{
-           items:1
-       }
-   }
+   navText: ["",""]
   });
+
+  // check when window scrolls
+  function checkScroll() {
+
+    // log
+    console.log('scrollin');
+
+    // variables
+    scrollPosition = $(document).scrollTop();
+
+    // check if we are half way
+    if(scrollPosition >= (windowHeight / 2) && ! $('body').hasClass('popup-show') ){
+      $('body').toggleClass('popup-show popup-hide');
+    } else if(scrollPosition < (windowHeight / 2) && $('body').hasClass('popup-show') ){
+      $('body').toggleClass('popup-show popup-hide');
+    }
+
+
+  }
+
+  // gets variables
+  function getVariables() {
+
+    // log
+    console.log('getVariables()');
+
+    // variables
+    windowHeight = $(document).outerHeight();
+
+  }
+
+
+  // Bind events
+  $(document).on('scroll', checkScroll);
+  $(window).on('load resize', getVariables);
+
 });
